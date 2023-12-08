bec1118985d9a8bad8c43a46be96d7c7c10c9801
iammdsafin <ahmedshafinshafi@gmail.com>
2023-12-08 23:16:16 +0600

    changed defult bg color, favicon, logo and add notpokeme component

--- class-1/class-one-project/src/App.tsx
+++ class-1/class-one-project/src/App.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "./functionalComponets/pokeButton";
+import PokeButton from "./functionalComponets/pokeButton";
+import NotPokeMe from "./functionalComponets/notPokeButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>Hello Planet Earth! Let's explore and learn React!</p>
-        <Button/>
+
+        <div className="pokeme-button">
+          <PokeButton />
+        </div>
+
+        <div className="notpokeme-button">
+          <NotPokeMe />
+        </div>
+
       </header>
     </div>
   );
