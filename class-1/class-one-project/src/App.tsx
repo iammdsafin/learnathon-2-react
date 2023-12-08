448f508d6831fd222b709f089e43a831d4caeadb
iammdsafin <ahmedshafinshafi@gmail.com>
2023-12-08 21:37:59 +0600

    tailwindcss added to class-one-project

--- class-1/class-one-project/src/App.tsx
+++ class-1/class-one-project/src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Planet Earth! Let's explore and learn React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello Planet Earth! Let's explore and learn React!</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Poke Me!
+        </button>
       </header>
     </div>
   );
