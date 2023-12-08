40d56515a294f6eaef4a4d48df8f63c27d989142
iammdsafin <ahmedshafinshafi@gmail.com>
2023-12-08 21:51:49 +0600

    PokeButton component added

--- class-1/class-one-project/src/App.tsx
+++ class-1/class-one-project/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./functionalComponets/pokeButton";
 
 function App() {
   return (
@@ -8,9 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Planet Earth! Let's explore and learn React!</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-          Poke Me!
-        </button>
+        <Button/>
       </header>
     </div>
   );
