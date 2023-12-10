3d3f02ac3c186206bed39bb9a0a8fb090520446d
iammdsafin <ahmedshafinshafi@gmail.com>
2023-12-10 15:25:34 +0600

    useStateFunctionalComponet added

--- class-2/class-two-project/src/components/PropsFunctionalComponent.js
+++ class-2/class-two-project/src/components/PropsFunctionalComponent.js
@@ -1,10 +1,16 @@
-export function  PropsFunctionalComponent(props) {
-    return (
-        <div>
-            <h1>Planet Earth Informations</h1>
-            <p>Name: {props.name}</p>
-            <p>Gravity: {props.gravity}</p>
-            <p>Population: {props.population}</p>
-        </div>
-    )
+import { useState } from "react";
+
+export function PropsFunctionalComponent(props) {
+  //usindg state in functional component
+  const [rotation, setCount] = useState("24.0 hours");
+
+  return (
+    <div>
+      <h1>Planet Earth Informations</h1>
+      <p>Name: {props.name}</p>
+      <p>Gravity: {props.gravity}</p>
+      <p>Population: {props.population}</p>
+      <p>Earth Rotation: {rotation}</p>
+    </div>
+  );
 }
